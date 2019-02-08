c713a17727ebacec854715cdf47230c1b9e8051f
HanByulKang <hanstar@designme.ai>
2019-02-08 15:30:20 +0900

    초기 커밋.

---
+++ ListScrollResearch/ListResearch.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+
+namespace ListScrollResearch
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class ListResearch : Page
+    {
+        public ObservableCollection<DateTest> DateTests { get; set; }
+        public ObservableCollection<DateTest> NowRenderedList { get; set; } = new ObservableCollection<DateTest>();
+
+        public ListResearch()
+        {
+            this.InitializeComponent();
+            //NowRenderedList = new List<DateTest>();
+            TestListViewCollection.Source = new DateTest().SetInitData();
+        }
+
+        private void TestListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < TestListView.Items.Count; i++)
+            {
+                var nowDateTest = TestListView.Items[i] as DateTest;
+                if (nowDateTest.Date.Date == DateTimeOffset.Now.Date)
+                {
+                    TestListView.SelectedIndex = i;
+                    TestListView.ScrollIntoView(TestListView.Items[i]);
+                    break;
+                }
+            }
+        }
+
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void TestListView_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            var ss = args.Handled;
+            var ss1 = args.InRecycleQueue;
+            var ss2 = args.Item as DateTest;
+            var ss3 = args.ItemContainer;
+            var ss4 = args.ItemIndex;
+            var ss5 = args.Phase;
+
+            //Debug.WriteLine(ss);
+            //Debug.WriteLine(ss1);
+            //Debug.WriteLine(ss2.Name);
+            //Debug.WriteLine(ss3);
+            //Debug.WriteLine(ss4);
+            //Debug.WriteLine(ss5);
+            //Debug.WriteLine("------------------------------");
+
+            CheckRecycleTest(ss1, ss2);
+            //args.RegisterUpdateCallback(this.CheckRecycleTest);
+            //args.Handled = true;
+        }
+
+        /// <summary>
+        /// 콜백
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void CheckRecycleTest(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            //Debug.WriteLine("IsRecycle : " + isRecycle.ToString() + "/ DateTest : " + dateTest.Name);
+            var dateTest = args.Item as DateTest;
+            if (args.InRecycleQueue)      // 다시 가상화 되는 상태
+            {
+                Debug.WriteLine("Removed");
+                NowRenderedList.Remove(dateTest);
+            }
+            else
+            {
+                Debug.WriteLine("Added");
+                NowRenderedList.Add(dateTest);
+            }
+
+            NowCount.Text = NowRenderedList.Count.ToString();            
+        }
+
+        /// <summary>
+        /// 실제 동작함
+        /// </summary>
+        /// <param name="isRecycle"></param>
+        /// <param name="dateTest"></param>
+        public void CheckRecycleTest(bool isRecycle, DateTest dateTest)
+        {
+            Debug.WriteLine("IsRecycle : " + isRecycle.ToString() + "/ DateTest : " + dateTest.Name);
+            if (isRecycle)      // 다시 가상화 되는 상태
+            {
+                Debug.WriteLine("Removed");
+                NowRenderedList.Remove(dateTest);
+            }
+            else
+            {
+                Debug.WriteLine("Added");
+                NowRenderedList.Add(dateTest);
+            }
+
+            NowCount.Text = "Now rendered items count : " + NowRenderedList.Count.ToString();
+            ContentChangeTest.Text = "Top Item = " + NowRenderedList.OrderBy(x => x.Date).FirstOrDefault().Name;
+            
+        }
+    }
+}
---
+++ ListScrollResearch/TestCases.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ListScrollResearch
+{
+    public class DateTest : INotifyCollectionChanged
+    {
+        // Test case
+        private ObservableCollection<DateTest> _testCases = new ObservableCollection<DateTest>();
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private int _testCount = 3000;
+
+        public ObservableCollection<DateGroup> SetInitData()
+        {
+            ObservableCollection<DateGroup> TestCasesGroup = new ObservableCollection<DateGroup>();
+
+            DateGroup now = new DateGroup
+            {
+                Key = DateTime.Now.ToString()
+            };
+
+            DateGroup before = new DateGroup
+            {
+                Key = DateTime.Now.AddDays(-1).ToString()
+            };
+
+            DateGroup after = new DateGroup
+            {
+                Key = DateTime.Now.AddDays(1).ToString()
+            };
+
+            for (int i = 0; i < _testCount; i++)
+            {
+                if (i < (_testCount / 3))
+                {
+                    _testCases.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now.AddDays(-1) });
+                    before.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now.AddDays(-1) });
+                }
+                else if (i > (_testCount / 3 - 1) && i < (_testCount / 2))
+                {
+                    _testCases.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now });
+                    now.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now });
+                }
+                else
+                {
+                    _testCases.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now.AddDays(1) });
+                    after.Add(new DateTest() { Name = "test_" + i, Date = DateTime.Now.AddDays(1) });
+                }
+            }
+
+            TestCasesGroup.Add(before);
+            TestCasesGroup.Add(now);
+            TestCasesGroup.Add(after);
+
+            return TestCasesGroup;
+        }
+
+
+        public DateTimeOffset Date { get; set; }
+        public string Name { get; set; }
+    }
+
+    /*
+     * 
+     * DateGroup List<object>
+	    - Key(*DateTest-Date와 일치해야 Group화 가능)
+		    ○ DateTest
+			    § Name
+			    § Date(*Key와 일치해야 함)
+     */
+    public class DateGroup : List<object>
+    {
+        public object Key { get; set; }
+    }
+}
